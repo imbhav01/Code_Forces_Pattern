4d9126909220132256cecd1ac63310b72f301d70
Bhavesh Anand Bajpai <bhaveshab01@gmail.com>
2023-03-24 21:48:41 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,16 @@
+n = 5
+num = 1
+k = 2*n - 2
+for i in range(0,n):
+    num = 1
+    for j in range(0,k):
+        print(end=" ")
+        
+    k = k - 2
+    
+    for j in range(0,i+1):
+        print(num)
+        num = num +1
+        
+    
+    
